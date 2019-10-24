38fff4995d264372e06df96506bc00577d4623d0
masih1999 <masih1999@hotmail.com>
2019-10-24 15:01:05 +0000

    Developing BMI calculator

--- 0.setup/bot3.py
+++ 0.setup/bot3.py
@@ -1,3 +1,8 @@
-print("What is your name?")
+print("What is your name?\n")
 name = input()#
-print("Hello " + name + ", nice to meet you")
+print("\nHello " + name + ", nice to meet you")
+print("\nHow old are you?\n")
+age = input()
+print("\nYour age is " + age )
+height =float(input("\nHow tall are you?"))
+print("Your height is" + height)
