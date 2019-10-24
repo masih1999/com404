fe83a8e65227a63411e2a98cea030a1782cd0f17
masih1999 <masih1999@hotmail.com>
2019-10-24 14:08:14 +0000

    Updated code to display longer multi-line message

--- 0.setup/bot.py
+++ 0.setup/bot.py
@@ -1,1 +1,8 @@
 print("system failure imminent!")
+print("System reboot has been initiated")
+print()
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine")
+print()
+print("System online")
