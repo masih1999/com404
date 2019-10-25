4fea4269fdbca6988d6e100f6903d29667f2988f
masih1999 <masih1999@hotmail.com>
2019-10-25 14:07:05 +0000

    direction of paint

---
+++ 0.setup/bot4.py
@@ -0,0 +1,13 @@
+print("which direction should I paint?")
+direction = input()
+
+if (direction == ("up")):
+    print("\n Im painting in an upward direction")
+elif (direction == ("right")):
+    print("\n Im painting in the right direction")
+elif (direction == ("down")):
+    print("\n Im painting in the downward direction")
+elif (direction == ("left")):
+    print("\n Im painting in the left direction")
+else :
+    print("\nUnsure of direction")
