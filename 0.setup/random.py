ed49fe0c790ccccaa945249f644b57c50194debb
masih1999 <masih1999@hotmail.com>
2019-10-29 19:55:58 +0000

    while loops

---
+++ 0.setup/random.py
@@ -0,0 +1,6 @@
+print("how old are you")
+age = int(input())
+
+if (age>=18):
+    print("youre old enough")
+else: print("too young sorry")
---
+++ whileloops.py
@@ -0,0 +1,6 @@
+iteration = 0
+print("starting iteration...")
+while (iteration<=10):
+    print(iteration)
+    iteration = iteration + 1
+print ("completed", iteration, ("!"))
